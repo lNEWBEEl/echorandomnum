af09e4eba6602aef63ad9cf9bd082127ed82d392
lNEWBEEl <37254812+lNEWBEEl@users.noreply.github.com>
2023-09-04 18:03:56 +0800

    Update and rename index.js to randomnum.js

---
+++ randomnum.js
@@ -0,0 +1,10 @@
+// randomnum.js
+
+// 生成一个随机数
+var randomNumber = Math.floor(Math.random() * 100); // 生成0到99之间的随机整数
+
+// 将随机数与字符串 "js" 结合起来
+var result = "js" + randomNumber;
+
+// 输出结果
+console.log(result);
